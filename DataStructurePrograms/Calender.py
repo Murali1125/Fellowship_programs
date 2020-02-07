93c2c630506d501e39510581d290c3b80ce7310b
murali1125 <murali1125s@gmail.com>
2020-02-07 14:59:36 +0530

    calender

---
+++ DataStructurePrograms/Calender.py
@@ -0,0 +1,74 @@
+"""-----------------------------------------------
+------------------Calender Module-----------------
+-----------------------------------------------"""
+
+class calender:
+
+    # getting day of the week od 1st day from given month and year
+    def day_of_the_week(self,year,month,day=1):
+        # assigning the year and month values to variable
+        y = year
+        m = month
+        d = day
+        y0 = y - (14 - m) / 12
+        x = y0 + y0 / 4 - y0 / 100 + y0 / 400
+        m0 = m + 12 * ((14 - m) / 12) - 2
+        d0 = int((d + x + (31 * m0 / 12))) % 7
+        # days = {1: "Sunday", 2: "Monday", 3: "Tuesday", 4: "Wednesday", 5: "Thursday", 6: "Friday", 7: "Saturday"}
+        # print(days[d0])
+        return d0
+    def days(self,year,month):
+        if month in (1,3,5,7,8,10,12):
+            return 31
+        elif month in (4,6,9,11):
+            return 30
+        elif month == 2:
+            if calender.leap_year(self,year):
+                return 29
+            else:
+                return 28
+        else:
+            print("wrong input of the month \n the month should be in range 01 to 12 \n you given{}".format(month))
+    # function to check the given yr is leap year or not if yes returns True else return False
+    def leap_year(self,year):
+        if  year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    return True
+                else:
+                    return False
+            else:
+                  return True
+        else:
+            return False
+
+    def month(self,year,month):
+        # d is the number of the weak-day of 1st date in the month
+        d = calender.day_of_the_week(self,year,month)
+        # getting the number of days in the month
+        days = calender.days(self,year,month)
+        # declaring an empty array to store the values
+        month = []
+        # taking an day iterator and initialize to 1
+        day = 1
+        # in the fallowing for-loop i represent the weak of the month
+        weak_days = ['Su','Mo','Tu','We','Th','Fr','Sa']
+        month.append(weak_days)
+        for i in range(0,5):
+            # creating temporary list to store the days of the a week
+            temp = []
+            # in the fallowing for-loop j represents the day of the weak
+            for j in range(0,7):
+                # if the day iterator > max days of the month break the loop
+                if day > days:
+                   temp.append("")
+                if i == 0 and j < d:
+                    temp.append("  ")
+                    continue
+                if day < 10:
+                    temp.append(f'0{day}')
+                else:
+                    temp.append(f'{day}')
+                day +=1
+            month.append(temp)
+        return month
---
+++ DataStructurePrograms/print_calender.py
@@ -0,0 +1,23 @@
+"""-----------------------------------------------------------------------------
+--> Write a program  that takes the month and year as command-line arguments and
+--prints the Calendar of the month. Store the Calendar in an 2D Array, the first
+--dimension the week of the month and the second dimension stores the day of the
+--week. Print the result as following
+-----------------------------------------------------------------------------"""
+from Fellowship_programs.DataStructurePrograms.Calender import calender
+
+#creating object for calender class
+obj_cal = calender()
+# reading year and month from user
+year = int(input("Enter the year"))
+month = int(input("Enter the month"))
+# declaring months dictionary
+m = {1:"January",2:"February",3:"March",4:"Aprial",5:"May",6:"June",7:"July",8:"Auguest",9:"September",10:"Octomber",11:"November",12:"December"}
+
+cal = obj_cal.month(year,month)
+print("Calender ")
+print()
+print("{} {}  ".format(year,m[month]))
+# printing the calender
+for i in cal:
+    print(i)
